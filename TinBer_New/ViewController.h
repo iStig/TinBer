81f6be20465311808f0c13331a8d60c516fbcd75
shentingting <shentingting@MacBook-ProT.local>
2015-07-06 18:15:35 +0800

    Initial Commit

---
+++ TinBer_New/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TinBer_New
+//
+//  Created by shentingting on 15/7/6.
+//  Copyright (c) 2015年 shentingting. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TinBer_New/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TinBer_New
+//
+//  Created by shentingting on 15/7/6.
+//  Copyright (c) 2015年 shentingting. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
